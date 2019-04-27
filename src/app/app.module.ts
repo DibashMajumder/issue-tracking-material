4c603f1e89f6cec705c2c5d07cd27fb56055cb34
Dibash7 <dibashmajumder123456@gmail.com>
2019-04-27 14:25:21 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
+import { UserModule } from './user/user.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RegisterComponent } from './user/register/register.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
+import { MyOwnCustomMaterialModuleModule } from './my-own-custom-material-module/my-own-custom-material-module.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    IssueListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    UserModule,
+    RouterModule.forRoot([
+      {path:'login', component: LoginComponent, pathMatch: 'full'},
+      {path:'', redirectTo: 'login', pathMatch: 'full'},
+      {path:'list', component: IssueListComponent },
+      {path:'404', component: NotFoundComponent },
+      {path:'*', redirectTo: '404' },
+      {path:'**', redirectTo: '404' }, 
+      {path:'view', component: LoginComponent }
+    ]),
+    MyOwnCustomMaterialModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
